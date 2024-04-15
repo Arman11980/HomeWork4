762fe82ed872f8840e09ede6599833119ca01645
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-04-15 20:08:32 +0300

    Update HomeWork4.py

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,7 +1,7 @@
 immutable_var = (1, 2, 'a', 'b')
 print(immutable_var)
-immutable_var[1][1] = 200 # кортеж менять нельзя-это закон
-print(immutable_var)
+immutable_var[1] = 200 # кортеж менять нельзя-это закон
+
 Mutable_list = [1, 2,'a','b', 'Modifiet']
-Mutable_list[0][0] = 8 # тот же закон
+Mutable_list[0] = 8 
 print(Mutable_list)
